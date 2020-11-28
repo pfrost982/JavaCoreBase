bc3b41dd9f558b4eae93d4d174800e3a7096bbc0
pfrost982 <power982@gmail.com>
2020-11-28 21:30:26 +0700

    Домашнее задание 3

--- src/lesson3/GuessNumber.java
+++ src/lesson3/GuessNumber.java
@@ -1,2 +1,46 @@
-package lesson3;public class GuessNumber {
+package lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+    
+    public static void main(String[] args) {
+        int num;
+        do {
+            game();
+            num = getNum("Повторить игру еще раз? 1 – да / 0 – нет (1 – повторить, 0 – нет): ");
+        } while (num == 1);
+    }
+
+    public static int getNum(String s) {
+        int num;
+        do {
+            System.out.print(s);
+            num = scanner.hasNextInt() ? scanner.nextInt() : -1;
+            if (num == -1) scanner.nextLine();
+        } while(num == -1);
+        return num;
+    }
+
+    public static void game() {
+        int rand = random.nextInt(10);
+        int tryNum = 1;
+        int answ;
+        while (tryNum < 4) {
+            System.out.println("Загадано: " + rand + ", Попытка № " + tryNum); //Показываю загаданное число для отладки
+            answ = getNum("Введите число от 0 до 9: ");
+            if (answ > rand) System.out.println("Ваше число больше загаданного");
+            else if (answ < rand) System.out.println("Ваше число меньше загаданного");
+            else if (answ == rand) {
+                System.out.println("Поздравляем вы угадали!!!");
+                tryNum = 5;
+            }
+            tryNum++;
+        }
+        if (tryNum == 4) System.out.println("К сожалению у вас закончились попытки...");
+    }
+
 }
--- src/lesson3/GuessWord.java
+++ src/lesson3/GuessWord.java
@@ -1,2 +1,50 @@
-package lesson3;public class GuessWord {
+package lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessWord {
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+        String[] words = {"apple", "orange", "lemon", "banana",
+                          "apricot", "avocado", "broccoli", "carrot",
+                          "cherry", "garlic", "grape", "melon",
+                          "leak", "kiwi", "mango", "mushroom", "nut",
+                          "olive", "pea", "peanut", "pear", "pepper",
+                          "pineapple", "pumpkin", "potato"};
+        int rand = random.nextInt(words.length);
+        String answ = "";
+        do {
+            System.out.println("Загадано: " + words[rand]); //Показываю загаданное слово для отладки
+            answ = getWord("Введите слово: ");
+            if (!answ.equals(words[rand])) System.out.println(fillMake(words[rand], answ));
+
+        } while (!answ.equals(words[rand]));
+        System.out.println("\nПоздравляю, вы угадали слово!!!");
+
+    }
+
+    public static String getWord(String s) {
+        System.out.print(s);
+        return scanner.nextLine();
+    }
+
+    public static String fillMake(String word, String answ) {
+        String fill = "";
+        int wordLength;
+        int i = 0;
+        if (word.length() > answ.length()) wordLength = answ.length();
+        else wordLength = word.length();
+
+        for (; i < wordLength; i++) {
+            if (word.charAt(i) == answ.charAt(i)) fill = fill + word.charAt(i);
+            else fill = fill + "#";
+        }
+        for (; i < 15 ; i++) fill = fill + "#";
+
+        return fill;
+    }
 }
