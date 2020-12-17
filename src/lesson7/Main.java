cce22aa9d2b554aae268a4dc8164c6c4be3c85b7
pfrost982 <power982@gmail.com>
2020-12-18 02:06:26 +0700

    еще 7

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -1,2 +1,36 @@
-package lesson7;public class Main {
+package lesson7;
+
+public class Main {
+    final static int CATCOUNT = 10;
+
+    public static void main(String[] args) {
+        Cat[] cats = new Cat[CATCOUNT];
+        String[] names = {"Макс", "Бейли", "Чарли", "Бадди", "Рокки", "Джейк", "Джек", "Тоби", "Коди", "Бустер", "Герцог",
+                "Купер", "Райли", "Харлей", "Биар", "Такер", "Мерфи", "Лаки", "Оливер", "Сэм", "Оскар", "Тедди",
+                "Уинстон", "Сэмми", "Шэдоу", "Гизмо", "Бэнтли", "Зевс", "Джексон", "Бакстер", "Бандит", "Гас",
+                "Самсон", "Мило", "Руди", "Луи", "Хантер", "Кейси", "Рокко", "Спаркли", "Джоуи", "Бруно", "Бо",
+                "Дакота", "Максимус", "Ромео", "Бумер", "Люк", "Генри"};
+        for (int i = 0; i < CATCOUNT; i++) {
+            cats[i] = new Cat(names[(int) (Math.random() * names.length)]);
+        }
+        System.out.println("У котов появился аппетит:\n");
+        for (int i = 0; i < CATCOUNT; i++) {
+            cats[i].setAppetite((int) (Math.random() * 10) + 1);
+            cats[i].info();
+        }
+        Plate plate = new Plate();
+        plate.addFood((int) (Math.random() * 100) + 1);
+        System.out.println();
+        plate.info();
+        System.out.println("Коты приступили к трапезе... \n");
+        for (int cat = 0; cat < CATCOUNT; cat++) {
+            cats[cat].eat(plate);
+        }
+        System.out.println();
+        plate.info();
+        System.out.println("Результат кормления:\n");
+        for (int i = 0; i < CATCOUNT; i++) {
+            cats[i].info();
+        }
+    }
 }
--- src/lesson7/Plate.java
+++ src/lesson7/Plate.java
@@ -1,2 +1,30 @@
-package lesson7;public class Plate {
+package lesson7;
+
+public class Plate {
+    private int food;
+
+    public Plate() {
+        this.food = 0;
+    }
+
+    public int eatFood(int food) {
+        int eatedFood;
+        if (this.food > food) {
+            this.food -= food;
+            return food;
+        } else {
+            eatedFood = this.food;
+            this.food = 0;
+            return eatedFood;
+        }
+    }
+
+    public void addFood(int food) {
+        this.food += food;
+    }
+
+    public void info() {
+        System.out.println("В тарелке " + food + " еды");
+    }
+
 }
