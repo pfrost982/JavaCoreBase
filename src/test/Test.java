952ac05132f8c93576e8cc83b7f11d34eb247aab
pfrost982 <power982@gmail.com>
2020-12-17 17:09:22 +0700

    Все планово, перед созданием homeworklesson7

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(5 + 5 + "3" + 5 + 5);
+        System.out.println("5" + 5 + 3 + 5 + 5);
+        System.out.println(5 + 5 + 3 + 5 + 5);
+        System.out.println(5 + 5 + 3 + "5" + 5);
+    }
+}
